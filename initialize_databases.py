bcc25bc400f5f464d4f351c2117f2c60497c879c
Ignacio Villanueva <ivillanueva@itba.edu.ar>
2021-07-16 21:01:37 -0300

    Added init db file

--- initialize_databases.py
+++ initialize_databases.py
@@ -1,12 +1,26 @@
-from src.PopulatePokemons import populate_pokemons_collection
+from pymongo import MongoClient
 
-print("Initializing MongoDB")
+from src.configurations import *
+from src.populate_players import populate_players
+from src.populate_pokemons import populate_pokemons_collection
 
-cli = pymongo.MongoClient(MONGODB_URI)
+import progressbar
+
+print("Initializing MongoDB\n")
+
+cli = MongoClient(MONGODB_URI)
 pokemon_c = cli[MONGODB_DB][MONGODB_POKEMONS_COLLECTION]
 player_c = cli[MONGODB_DB][MONGODB_PLAYERS_COLLECTION]
 
-print("Populating Pokemons...")
-populate_pokemons_collection()
-populate_players(player_c, pokemon_c)
+pokemon_c.create_index([('name', 1)], unique=True)
+player_c.create_index([('name', 1)], unique=True)
 
+print("Populating Pokemon...")
+pokemon_bar = progressbar.ProgressBar(widgets=[progressbar.SimpleProgress(), "\t-\t", progressbar.AdaptiveETA()]).start()
+populate_pokemons_collection(pokemon_c, bar=pokemon_bar)
+pokemon_bar.finish()
+print("Done.\n")
+
+print("Populating Players...")
+populate_players(player_c, pokemon_c)
+print("Done\n")
