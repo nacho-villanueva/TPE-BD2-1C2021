009e8ad4a8dbb7ea84dbf470e0d3c1bc196bde3c
Gabriel Silvatici <gsilvati@itba.edu.ar>
2021-07-19 23:15:31 -0300

    spawn pokemons call

--- src/pokemon.py
+++ src/pokemon.py
@@ -28,6 +28,20 @@ async def get_pokemon_type(pokemon_type: str, request: Request):
 
     raise HTTPException(status_code=404, detail=f"Pokemon {pokemon} not found")
 
+@pokemon_router.post("/spawn_pokemons", response_model=List[PokemonModel])
+async def spawn_pokemon(pokemon_type: str, coords: CoordinatesModel, request: Request):
+    pokemons = await request.app.state.mongodb[MONGODB_DB][MONGODB_POKEMONS_COLLECTION].aggregate([{ "$sample": { "size": 10 } }])
+    
+    if len(pokemons) > 0:
+        for pokemon in pokemons: #100m
+            new_coords = generate_nearby_pos(coords.lat, coords.long, 100)
+            await asyncio.wait([
+                request.app.state.redis.geoadd("pokemons", new_coords.lat, new_coords.long, "{nonce},{pokemon.name}"),
+                request.app.state.redis.setex(f"pokemons:{pokemon.name}:expire", POKEMON_EXPIRATION, "EXPIRE")            
+            ])    
+        return pokemons        
 
+    raise HTTPException(status_code=404, detail=f"Pokemons not found")
+    
 async def get_nearby_pokemons():
     pass
